606e53d572fde4152f72ee1dbb772e43015726ab
fgilet <francois.gilet@hotmail.fr>
2022-08-09 14:12:18 +0200

    improved the visual and handled network errors

--- app/src/main/java/com/example/breathe/LoadingActivity.java
+++ app/src/main/java/com/example/breathe/LoadingActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -23,37 +25,63 @@ public class LoadingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
 
+        // hint text view iat bottom of screen
+        TextView tv = findViewById(R.id.check_connection);
+
+        // creating and starting network requests queue
         RequestQueue queue = Volley.newRequestQueue(this);
         queue.start();
 
+        // list of available cities
         ArrayList<String> cities = new ArrayList<>();
 
+        // requesting the list of available cities
         MyJsonArrayRequest requestListOfCities = new MyJsonArrayRequest
                 (Request.Method.GET, "https://api.aircheckr.com/v1.5/territory/BE/names", null, new Response.Listener() {
                     @Override
                     public void onResponse(Object response) {
                         JSONArray array = (JSONArray) response;
+                        // iterating through every city in the Json file
                         for (int i = 0; i < array.length(); i++) {
                             try {
                                 JSONArray array2 = array.getJSONObject(i).getJSONArray("name");
                                 for (int j = 0; j < array2.length(); j++) {
+                                    // adding the name of the city to the list
                                     cities.add(array2.get(j).toString());
                                 }
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
                         }
+                        // list of cities is complete
+                        // launching main activity and sending it the list of cities
                         Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
                         intent.putExtra("cities", cities);
                         startActivity(intent);
+                        // closing the root activity and therefore the app on return from main activity
+                        finish();
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        System.out.println("ERROR RESPONSE !!!");
+                        // in case of error response
+                        // setting it clickable
+                        // changing hint text
+                        tv.setClickable(true);
+                        tv.setText(R.string.check_connection);
                         error.printStackTrace();
                     }
                 });
+
+        // defining hint text view behavior
+        tv.setOnClickListener(view -> {
+            // setting it not clickable
+            // updating hint text
+            // sending the request again
+            tv.setClickable(false);
+            tv.setText(R.string.loading);
+            queue.add(requestListOfCities);
+        });
 
         queue.add(requestListOfCities);
     }
--- app/src/main/java/com/example/breathe/MainActivity.java
+++ app/src/main/java/com/example/breathe/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.graphics.drawable.DrawableCompat;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -42,63 +44,83 @@ import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
+    // instantiating network requests queue and visual objects
     private RequestQueue queue;
     AutoCompleteTextView field;
     Button button;
     TextView city;
     TextView quality;
     TextView advice;
+    View margins;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // creating and starting network requests queue
         queue = Volley.newRequestQueue(this);
         queue.start();
 
-        AutoCompleteTextView field = findViewById(R.id.field);
+        // linking java visual objects to XML components
+        field = findViewById(R.id.field);
         button = findViewById(R.id.button);
         city = findViewById(R.id.city);
         quality = findViewById(R.id.quality);
         advice = findViewById(R.id.advice);
+        margins = findViewById(R.id.margins);
 
+        // retrieving list of cities from loading activity
         ArrayList<String> cities = (ArrayList<String>) getIntent().getSerializableExtra("cities");
 
+        // setting content of the drop down list for autocomplete
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cities);
         field.setAdapter(adapter);
 
+        //setting search button behavior
         button.setOnClickListener(view -> {
+            // retrieving selected city
             String c = field.getText().toString();
+            // if the name is in the list of available cities
             if(cities.contains(c)) {
+                // retrieve information and update screen
                 getQuality(c);
-            } else {
+            } else { // if the name is not in the list
+                // clearing info from potential previous search
                 city.setText("");
                 quality.setText("");
                 advice.setText("");
-                Toast.makeText(MainActivity.this,"Please select a city from the drop down list.", Toast.LENGTH_LONG).show();
+                margins.setBackgroundColor(getResources().getColor(R.color.theme));
+                // showing toast message to warn user
+                Toast.makeText(MainActivity.this,R.string.toast_dropdown, Toast.LENGTH_LONG).show();
             }
         });
     }
 
     private void getQuality(String c) {
+        /**
+         * This method receives the name of a city supposedly available in the database with the spelling provided.
+         * It requests data about the air quality and updates the screen accordingly.         *
+         */
 
+        // building the url for the request
         String url = "https://api.aircheckr.com/v1.5/territory/BE/LAU2/name/" + c;
-        System.out.println(url);
 
+        // creating the request
         MyJsonObjectRequest request = new MyJsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
                     @Override
-                    public void onResponse(JSONObject response) {
+                    public void onResponse(JSONObject response) { // on successful response
                         try {
-                            city.setText("City : " + c);
-                            quality.setText("Quality : " + response.getJSONArray("data").getJSONObject(0).getJSONObject("aqi_11").getString("name"));
+                            // updating screen components with the information received
+                            city.setText(getString(R.string.city, c));
+                            quality.setText(getString(R.string.quality, response.getJSONArray("data").getJSONObject(0).getJSONObject("aqi_11").getString("name")));
+                            advice.setText(getString(R.string.advice, response.getJSONArray("data").getJSONObject(0).getJSONObject("recommend_non_sensitive").getString("general")));
+                            // retrieving color code and updating the colored frame with it
                             String color = response.getJSONArray("data").getJSONObject(0).getJSONObject("aqi_11").getString("color_hex");
-                            Drawable unwrappedDrawable = AppCompatResources.getDrawable(MainActivity.this.getBaseContext(), R.drawable.square);
-                            Drawable wrappedDrawable = DrawableCompat.wrap(unwrappedDrawable);
-                            DrawableCompat.setTint(wrappedDrawable, Color.parseColor(color));
-                            advice.setText("Advice : " + response.getJSONArray("data").getJSONObject(0).getJSONObject("recommend_non_sensitive").getString("general"));
+                            margins.setBackgroundColor(Color.parseColor(color));
+                            // closing the keyboard to enhance readability of results
                             View view = MainActivity.this.getCurrentFocus();
                             if (view != null) {
                                 InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -111,15 +133,19 @@ public class MainActivity extends AppCompatActivity {
                 }, new Response.ErrorListener() {
 
                     @Override
-                    public void onErrorResponse(VolleyError error) {
+                    public void onErrorResponse(VolleyError error) { // on network error
+                        // clearing info from potential previous search
                         city.setText("");
                         quality.setText("");
                         advice.setText("");
-                        Toast.makeText(MainActivity.this,"Please check your internet connection and try again.", Toast.LENGTH_LONG).show();
+                        margins.setBackgroundColor(getResources().getColor(R.color.theme));
+                        // showing toast message to warn user of the network issue
+                        Toast.makeText(MainActivity.this,R.string.toast_connection, Toast.LENGTH_LONG).show();
                         error.printStackTrace();
                     }
                 });
 
+        // sending the request
         queue.add(request);
     }
 }
